ce557cca566c6d26fb75be2af532987db672f72f
Adekunle Bamzz <bams.kunle@gmail.com>
2026-03-22 19:38:52 +0100

    test accept valid Stacks addresses wrapped in tabs

---
+++ frontend/src/utils/isValidStacksAddress-tab-trim.test.js
@@ -0,0 +1,9 @@
+import { describe, expect, it } from 'vitest'
+import { isValidStacksAddress } from './strings'
+
+describe('isValidStacksAddress', () => {
+  it('accepts valid addresses wrapped with tab characters', () => {
+    const value = '\tSP5K2RHMSBH4PAP4PGX77MCVNK1ZEED07CWX9TJT\t'
+    expect(isValidStacksAddress(value)).toBe(true)
+  })
+})
