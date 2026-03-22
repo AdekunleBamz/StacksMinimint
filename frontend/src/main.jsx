a84c40cb85ba8261506fc985202ee28e25f635e1
Adekunle Bamzz <bams.kunle@gmail.com>
2026-03-22 18:16:46 +0100

    refactor remove redundant root error boundary

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import ErrorBoundary from './components/ErrorBoundary'
 import { ThemeProvider } from './context/ThemeContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
+      <App />
     </ThemeProvider>
   </React.StrictMode>
 )
