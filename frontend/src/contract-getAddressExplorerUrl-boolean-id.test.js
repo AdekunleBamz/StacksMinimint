b135acd27e080affef1c4a97c198d8602a979751
Adekunle Bamzz <bams.kunle@gmail.com>
2026-03-23 20:09:14 +0100

    test(contract): stringify boolean addresses in explorer URLs

---
+++ frontend/src/contract-getAddressExplorerUrl-boolean-id.test.js
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest'
+import { getAddressExplorerUrl } from './contract'
+
+describe('getAddressExplorerUrl', () => {
+  it('stringifies boolean address identifiers', () => {
+    expect(getAddressExplorerUrl(false)).toBe('https://explorer.hiro.so/address/false?chain=mainnet')
+  })
+})
