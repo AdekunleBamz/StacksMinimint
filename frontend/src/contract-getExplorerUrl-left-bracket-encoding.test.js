f507f964097017feaff199a5e93af6a26fca0bec
Adekunle Bamzz <bams.kunle@gmail.com>
2026-03-24 21:05:06 +0100

    test(contract): encode left bracket for transaction explorer links

---
+++ frontend/src/contract-getExplorerUrl-left-bracket-encoding.test.js
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest'
+import { getExplorerUrl } from './contract'
+
+describe('getExplorerUrl', () => {
+  it('encodes left bracket characters in transaction identifiers', () => {
+    expect(getExplorerUrl("tx[id")).toBe("https://explorer.hiro.so/txid/tx%5Bid?chain=mainnet")
+  })
+})
