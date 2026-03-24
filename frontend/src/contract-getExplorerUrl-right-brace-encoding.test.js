a17566f66d0d0e29d855a4ff830d1bc7b35bd64b
Adekunle Bamzz <bams.kunle@gmail.com>
2026-03-24 21:04:51 +0100

    test(contract): encode right brace for transaction explorer links

---
+++ frontend/src/contract-getExplorerUrl-right-brace-encoding.test.js
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest'
+import { getExplorerUrl } from './contract'
+
+describe('getExplorerUrl', () => {
+  it('encodes right brace characters in transaction identifiers', () => {
+    expect(getExplorerUrl("tx}id")).toBe("https://explorer.hiro.so/txid/tx%7Did?chain=mainnet")
+  })
+})
