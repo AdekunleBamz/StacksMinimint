2d60f101e522dd4843e76b4b06aede02d9d15703
Adekunle Bamzz <bams.kunle@gmail.com>
2026-03-24 21:28:24 +0100

    test(contract): encode left bracket for address explorer links

---
+++ frontend/src/contract-getAddressExplorerUrl-left-bracket-encoding.test.js
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest'
+import { getAddressExplorerUrl } from './contract'
+
+describe('getAddressExplorerUrl', () => {
+  it('encodes left bracket characters in wallet addresses', () => {
+    expect(getAddressExplorerUrl("SP5K[TJT")).toBe("https://explorer.hiro.so/address/SP5K%5BTJT?chain=mainnet")
+  })
+})
