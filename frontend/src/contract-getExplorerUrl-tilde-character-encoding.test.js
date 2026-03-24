499b1351c104a5b3febd8d635dbbe46cfbeb0100
Adekunle Bamzz <bams.kunle@gmail.com>
2026-03-24 22:01:47 +0100

    test(contract): cover tilde encoding in transaction explorer links

---
+++ frontend/src/contract-getExplorerUrl-tilde-character-encoding.test.js
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest'
+import { getExplorerUrl } from './contract'
+
+describe('getExplorerUrl', () => {
+  it('encodes tilde characters in transaction identifiers', () => {
+    expect(getExplorerUrl("tx~id")).toBe("https://explorer.hiro.so/txid/tx~id?chain=mainnet")
+  })
+})
