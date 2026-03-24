53b1e7ddee01842e01b26efc77b42333deb17605
Adekunle Bamzz <bams.kunle@gmail.com>
2026-03-24 22:02:01 +0100

    test(contract): cover close parenthesis encoding in address explorer links

---
+++ frontend/src/contract-getAddressExplorerUrl-close-parenthesis-encoding.test.js
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest'
+import { getAddressExplorerUrl } from './contract'
+
+describe('getAddressExplorerUrl', () => {
+  it('encodes close parenthesis characters in address identifiers', () => {
+    expect(getAddressExplorerUrl("SP5K)TJT")).toBe("https://explorer.hiro.so/address/SP5K)TJT?chain=mainnet")
+  })
+})
