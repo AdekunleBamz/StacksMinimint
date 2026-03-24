10f0531fc16e34391448a08c7225bb7e6db3445e
Adekunle Bamzz <bams.kunle@gmail.com>
2026-03-24 21:19:50 +0100

    test(contract): encode left bracket for token explorer links

---
+++ frontend/src/contract-getTokenExplorerUrl-left-bracket-encoding.test.js
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest'
+import { getTokenExplorerUrl } from './contract'
+
+describe('getTokenExplorerUrl', () => {
+  it('encodes left bracket characters in token identifiers', () => {
+    expect(getTokenExplorerUrl("token[id")).toBe("https://explorer.hiro.so/token/token%5Bid?chain=mainnet")
+  })
+})
