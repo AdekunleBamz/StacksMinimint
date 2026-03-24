ff212a738fdb38fc094c1d909ede1ef7fa69703e
Adekunle Bamzz <bams.kunle@gmail.com>
2026-03-24 22:02:00 +0100

    test(contract): cover open parenthesis encoding in address explorer links

---
+++ frontend/src/contract-getAddressExplorerUrl-open-parenthesis-encoding.test.js
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest'
+import { getAddressExplorerUrl } from './contract'
+
+describe('getAddressExplorerUrl', () => {
+  it('encodes open parenthesis characters in address identifiers', () => {
+    expect(getAddressExplorerUrl("SP5K(TJT")).toBe("https://explorer.hiro.so/address/SP5K(TJT?chain=mainnet")
+  })
+})
