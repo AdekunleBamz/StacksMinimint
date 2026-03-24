00055f0cc75670f602db5cac9d40fc33aa12f2f2
Adekunle Bamzz <bams.kunle@gmail.com>
2026-03-24 21:44:09 +0100

    test(contract): cover right brace encoding in address explorer links

---
+++ frontend/src/contract-getAddressExplorerUrl-right-brace-encoding.test.js
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest'
+import { getAddressExplorerUrl } from './contract'
+
+describe('getAddressExplorerUrl', () => {
+  it('encodes right brace characters in address identifiers', () => {
+    expect(getAddressExplorerUrl("SP5K}TJT")).toBe("https://explorer.hiro.so/address/SP5K%7DTJT?chain=mainnet")
+  })
+})
