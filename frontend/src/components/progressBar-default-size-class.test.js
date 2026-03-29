f70dd4e9530fffbbcd65dee639be27f1cc750e5b
Adekunle Bamzz <bams.kunle@gmail.com>
2026-03-29 17:45:13 +0100

    test: add scope note for progressBar-default-size-class

--- frontend/src/components/progressBar-default-size-class.test.js
+++ frontend/src/components/progressBar-default-size-class.test.js
@@ -4,6 +4,7 @@ import { renderToStaticMarkup } from 'react-dom/server'
 import { ProgressBar } from './ProgressBar'
 
 // Regression note: preserve progressBar default size class behavior coverage.
+// Scope note: validates progressBar default size class behavior for regressions.
 describe('ProgressBar', () => {
   it('uses medium sizing class by default', () => {
     const markup = renderToStaticMarkup(React.createElement(ProgressBar, { value: 10 }))
