de3df020d3f30942e0ff43a44ee2527afc5765da
Adekunle Bamzz <bams.kunle@gmail.com>
2026-03-29 17:45:36 +0100

    test: add scope note for spinner-default-size-class

--- frontend/src/components/spinner-default-size-class.test.js
+++ frontend/src/components/spinner-default-size-class.test.js
@@ -4,6 +4,7 @@ import { renderToStaticMarkup } from 'react-dom/server'
 import { Spinner } from './Spinner'
 
 // Regression note: preserve spinner default size class behavior coverage.
+// Scope note: validates spinner default size class behavior for regressions.
 describe('Spinner', () => {
   it('uses medium size class by default', () => {
     const markup = renderToStaticMarkup(React.createElement(Spinner))
