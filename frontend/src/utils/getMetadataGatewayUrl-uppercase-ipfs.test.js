a42084f03646e7d1037e15314058d079a9458953
Adekunle Bamzz <bams.kunle@gmail.com>
2026-03-29 17:27:51 +0100

    test: clarify regression intent for getMetadataGatewayUrl-uppercase-ipfs

--- frontend/src/utils/getMetadataGatewayUrl-uppercase-ipfs.test.js
+++ frontend/src/utils/getMetadataGatewayUrl-uppercase-ipfs.test.js
@@ -1,6 +1,7 @@
 import { describe, expect, it } from 'vitest'
 import { getMetadataGatewayUrl } from './collection'
 
+// Regression note: preserve getMetadataGatewayUrl uppercase ipfs behavior coverage.
 describe('getMetadataGatewayUrl', () => {
   it('converts uppercase IPFS schemes to gateway URLs', () => {
     expect(getMetadataGatewayUrl('IPFS://QmExampleCid')).toBe('https://ipfs.io/ipfs/QmExampleCid')
