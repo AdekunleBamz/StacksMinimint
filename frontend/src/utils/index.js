9d1d63faa999f6035b4afb684c968b47d5ee1489
AdekunleBamz <bams.kunle@gmail.com>
2026-04-07 12:52:30 +0100

    chore: add utils barrel export file for cleaner imports

---
+++ frontend/src/utils/index.js
@@ -0,0 +1,43 @@
+/**
+ * Utils barrel export file.
+ * 
+ * Centralizes all utility function exports for clean imports
+ * throughout the application.
+ */
+
+// String utilities
+export {
+  truncateAddress,
+  capitalize,
+  isValidStacksAddress
+} from './strings';
+
+// Collection utilities
+export {
+  MAX_TOKEN_URI_LENGTH,
+  formatSTX,
+  formatAddress,
+  formatLimit,
+  formatRelativeTime,
+  formatExactTime,
+  getMetadataKind,
+  getMetadataLabel,
+  getMetadataGatewayUrl,
+  validateTokenURI,
+  createSubmissionRecord,
+  getCardAccent
+} from './collection';
+
+// Focus trap utilities
+export {
+  createFocusTrap,
+  trapFocusOnce,
+  getFocusableElements
+} from './focusTrap';
+
+/**
+ * Default exports for individual utility modules.
+ */
+export { default as stringsDefault } from './strings';
+export { default as collectionDefault } from './collection';
+export { default as focusTrapDefault } from './focusTrap';
