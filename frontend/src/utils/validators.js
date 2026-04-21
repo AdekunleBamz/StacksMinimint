df6b6e8d3fdb4f65dc5e45c7c902445b3e418d03
AdekunleBamz <bams.kunle@gmail.com>
2026-04-21 14:41:47 +0100

    feat: add isValidBlockHeight validator

--- frontend/src/utils/validators.js
+++ frontend/src/utils/validators.js
@@ -8,3 +8,5 @@ export const isValidCID = (v) => typeof v === "string" && v.length >= 10;
 export const isValidRoyaltyBps = (v) => !isNaN(Number(v)) && Number(v) >= 0 && Number(v) <= 10000;
 
 export const isValidMetadataVersion = (v) => Number.isInteger(Number(v)) && Number(v) >= 1;
+
+export const isValidBlockHeight = (v) => Number.isInteger(Number(v)) && Number(v) >= 0;
