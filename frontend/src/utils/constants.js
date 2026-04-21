f10bbc309ce9225f0354dadec256a0b5a53f2153
AdekunleBamz <bams.kunle@gmail.com>
2026-04-21 14:38:24 +0100

    feat: add STACKS_API_BASE_URL constant

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -24,3 +24,5 @@ export const WHITELIST_BLOCK_START = 100000
 export const PUBLIC_MINT_BLOCK = 105000
 
 export const PROVENANCE_HASH_ALGORITHM = "sha256"
+
+export const STACKS_API_BASE_URL = "https://stacks-node-api.mainnet.stacks.co"
