3171dddd2b7a1c9d1df6a0f01720ba0d73730b4c
AdekunleBamz <bams.kunle@gmail.com>
2026-04-21 14:39:41 +0100

    feat: add formatRarity helper

--- frontend/src/utils/format.js
+++ frontend/src/utils/format.js
@@ -6,3 +6,5 @@ export const formatMintPrice = (stx) => stx + " STX";
 export const formatSupply = (minted, max) => minted + " / " + max;
 
 export const formatRoyalty = (bps) => (bps / 100).toFixed(1) + "%";
+
+export const formatRarity = (tier) => tier.charAt(0).toUpperCase() + tier.slice(1);
