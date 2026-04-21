20757f2100eb43b1e7dbc6184ba62fa4046a9862
AdekunleBamz <bams.kunle@gmail.com>
2026-04-21 14:41:46 +0100

    feat: add isValidCID validator

--- frontend/src/utils/validators.js
+++ frontend/src/utils/validators.js
@@ -2,3 +2,5 @@
 export const isValidTokenId = (v) => Number.isInteger(Number(v)) && Number(v) >= 0;
 
 export const isValidMintCount = (v) => Number.isInteger(Number(v)) && Number(v) >= 1;
+
+export const isValidCID = (v) => typeof v === "string" && v.length >= 10;
