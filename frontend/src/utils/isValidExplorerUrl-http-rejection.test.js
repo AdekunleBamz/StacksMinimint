149a7bb4bdf91cd0638332aabf528941be110d7c
AdekunleBamz <bams.kunle@gmail.com>
2026-04-30 23:47:15 +0100

    Add isValidExplorerUrl http rejection test

--- frontend/src/utils/isValidExplorerUrl-http-rejection.test.js
+++ frontend/src/utils/isValidExplorerUrl-http-rejection.test.js
@@ -5,4 +5,8 @@ describe('isValidExplorerUrl', () => {
   it('rejects non-https explorer links', () => {
     expect(isValidExplorerUrl('http://explorer.stacks.co/txid/abc')).toBe(false)
   })
+
+  it('rejects non-https explorer URLs', () => {
+    expect(isValidExplorerUrl('http://explorer.stacks.co/txid/123')).toBe(false)
+  })
 })
